58a477be53d2a3421b6b39996e9276d8c2f01f2f
Артём Поташов <a.potashov@region1.ru>
2025-01-22 15:25:00 +0200

    Cors setup

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 import uvicorn
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.api.auth import auth_router
 from src.api.lessons import lessons_router
 
 app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(auth_router)
 app.include_router(lessons_router)
 
